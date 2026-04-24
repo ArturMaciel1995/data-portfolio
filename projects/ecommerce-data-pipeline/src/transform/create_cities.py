a5d4e88fb86b8bf8e331d4e561a600e3ed00cdde
Artur Maciel <arturmaciel1995@outlook.com>
2026-04-24 19:59:57 -0300

    refactor: standardize cities transformation script

--- projects/ecommerce-data-pipeline/src/transform/create_cities.py
+++ projects/ecommerce-data-pipeline/src/transform/create_cities.py
@@ -1,25 +1,28 @@
 import pandas as pd
 
-customers = pd.read_csv("../../data/raw/olist_customers_dataset.csv")
+# Extract
+customers = pd.read_csv('../../data/raw/olist_customers_dataset.csv')
 
+# Transform
 cities_df = customers[['customer_city', 'customer_state']].copy()
 
+# Padronização para evitar duplicidade
 cities_df['customer_city'] = cities_df['customer_city'].str.upper()
 cities_df['customer_state'] = cities_df['customer_state'].str.upper()
 
-cities_unique = cities_df.drop_duplicates()
-cities_unique = cities_unique.reset_index(drop=True)
+# Remover duplicados
+cities_unique = cities_df.drop_duplicates().reset_index(drop=True)
 
+# Surrogate key
 cities_unique['city_sk'] = cities_unique.index + 1
 
+# Renomear para padrão da dimensão
 cities_unique = cities_unique.rename(columns={
-    "customer_city": "city_name",
-    "customer_state": "state"
+    'customer_city': 'city_name',
+    'customer_state': 'state'
 })
 
 cities_unique = cities_unique[['city_sk', 'city_name', 'state']]
 
-print(cities_unique.head())
-print(cities_unique.shape)
-
-cities_unique.to_csv("../../data/processed/cities.csv", index=False)
+# Load (arquivo processed)
+cities_unique.to_csv('../../data/processed/cities.csv', index=False)
